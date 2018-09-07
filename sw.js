dc0e918f7831bf7cc1d643131e53df7e368bac32
Jerome Etienne <jerome.etienne@gmail.com>
2018-09-07 21:34:45 +0100

    better docs

--- sw.js
+++ sw.js
@@ -20,11 +20,11 @@ workbox.core.setLogLevel(workbox.core.LOG_LEVELS.debug)
 //////////////////////////////////////////////////////////////////////////////
 //		register some routes
 //////////////////////////////////////////////////////////////////////////////
-// workbox.routing.registerRoute(
-// 	new RegExp('.*\.js'),
-// 	workbox.strategies.cacheFirst()
-// )
-// 
+workbox.routing.registerRoute(
+	new RegExp('.*\.js'),
+	workbox.strategies.cacheFirst()
+)
+
 workbox.routing.registerRoute(
 	new RegExp('index.html'),
 	workbox.strategies.cacheFirst()
@@ -34,12 +34,10 @@ workbox.routing.registerRoute(
 	new RegExp('/'),
 	workbox.strategies.cacheFirst()
 )
-// 
-// // TODO register / for index.html
-// 
-// //////////////////////////////////////////////////////////////////////////////
-// //		precache and route
-// //////////////////////////////////////////////////////////////////////////////
-// 
-// // TO BE FILLED BY ``````
-// workbox.precaching.precacheAndRoute([])
+
+//////////////////////////////////////////////////////////////////////////////
+//		precache and route
+//////////////////////////////////////////////////////////////////////////////
+
+// TO BE FILLED BY ```workbox injectManifest workbox-config.js```
+workbox.precaching.precacheAndRoute([])
